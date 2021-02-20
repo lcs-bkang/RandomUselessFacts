4447da4c96b2fb190544ca9631ca18b2a4b856ad
Bradley Kang <bkang@lcs.on.ca>
2021-02-20 10:58:20 -0500

    Initial Commit

---
+++ RandomUselessFacts/RandomUselessFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUselessFactsApp.swift
+//  RandomUselessFacts
+//
+//  Created by Brad Kang on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct RandomUselessFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
